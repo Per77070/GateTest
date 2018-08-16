9a4e03af61ee167b30e5e6911764a2e17cd59d73
Per77070 <32507684+Per77070@users.noreply.github.com>
2018-08-16 22:00:06 +0200

    Add files via upload

---
+++ Talinmatning.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Talinmatning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+
+
+	int num=0;
+	do 
+	{
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Skriv in ett tal: ");
+ num= scanner.nextInt();
+ 
+		
+	}
+	while(num!=0);
+	return;
+	
+	}}
+		
+	
+
+	
+	
+	
+	
+
+
